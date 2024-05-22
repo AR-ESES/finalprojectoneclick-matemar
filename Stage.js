e41933401dffef4839e78244fd2700395fd5dab4
beacatita <161735314+beacatita@users.noreply.github.com>
2024-05-22 22:13:28 +0100

    Stage.js

--- Stage.js
+++ Stage.js
@@ -1,14 +1,3 @@
-/* eslint-disable require-yield, eqeqeq */
-
-import {
-  Stage as StageBase,
-  Trigger,
-  Watcher,
-  Costume,
-  Color,
-  Sound,
-} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
-
 export default class Stage extends StageBase {
   constructor(...args) {
     super(...args);
@@ -23,19 +12,19 @@ export default class Stage extends StageBase {
     this.sounds = [new Sound("Pop!", "./Stage/sounds/Pop!.wav")];
 
     this.triggers = [
-      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
-      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked2),
+      new Trigger(Trigger.BotãoInicialLinha, this.whenBotãoInicialLinhaClicked),
+      new Trigger(Trigger.BotãoInicialLinha, this.whenBotãoInicialLinhaClicked2),
     ];
 
     this.vars.scroll = -1093;
     this.vars.resultado = 0;
   }
 
-  *whenGreenFlagClicked() {
+  *whenBotãoInicialLinhaClicked() {
     this.vars.scroll = 0;
   }
 
-  *whenGreenFlagClicked2() {
+  *whenBotãoInicialLinhaClicked2() {
     while (true) {
       this.vars.scroll--;
       yield;
