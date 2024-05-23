3b896622a49be6f229640f2e753a18ea061051f4
beacatita <161735314+beacatita@users.noreply.github.com>
2024-05-23 12:56:36 +0100

    Botão_inicia_linhas.js

--- Botão_inicia_linhas.js
+++ Botão_inicia_linhas.js
@@ -1,1 +1,9 @@
+<div id="launch" class="screen" hidden title="Click to start">
+	<div class="start">
+this.costumes = [
+      new Costume("Botão_inicia_linhas", "./Botão_inicia_linhas/costumes/Botão inicio - linhas.png", {
+        x: 520,
+        y: 205,
+      }),
+    ];
 
