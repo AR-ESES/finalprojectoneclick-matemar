0fe016dae524196f664c4edcd9d0def9374339e1
beacatita <161735314+beacatita@users.noreply.github.com>
2024-05-23 12:06:52 +0100

    41.js

--- 41.js
+++ 41.js
@@ -5,11 +5,11 @@ this.costumes = [
     this.sounds = [];
 
     this.triggers = [
-      new Trigger(Trigger.GREEN_FLAG, this.whenGreenFlagClicked),
+      new Trigger(Trigger.Botão_Inicio_linhas, this.whenBotão_Inicio_linhasClicked),
     ];
   }
 
-  *whenGreenFlagClicked() {
+  *whenBotão_Inicio_linhasClicked() {
     this.visible = false;
     this.goto(250, -17);
     yield* this.wait(26);
